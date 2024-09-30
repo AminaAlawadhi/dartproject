72bbf84ef558ae6198d6191910bd9dbee655f29b
AminaAlawadhi <amina7alawadhi@gmail.com>
2024-09-30 10:03:43 +0300

    insert commit

--- main.dart
+++ main.dart
@@ -4,5 +4,7 @@ void main(){
   //Fahrenheit to Celsius:   (°F − 32) / 1.8 = °C
   double tempInCelsius = (tempInFahrenheit-(32))/1.8 ;
   print("$tempInFahrenheit °F = $tempInCelsius °C" );
-
+ 
+  //Celsius to Fahrenheit: °C * 1.8000 + 32.00 = °F
+  
 }
